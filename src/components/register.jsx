936e028d9a3005526dccead3548e73eccdbc72c0
Farshid <xamanifar98@gmail.com>
2022-11-16 18:34:59 +0330

    ref in classlist componenet

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,22 +1,51 @@
-import React, { Component } from "react";
+import axios from "axios";
+import React, { Component, createRef, Ref } from "react";
 
 class Register extends Component {
-    handlesubmit =(e)=>{
-        e.preventDefault();
-    
+  email = createRef();
+  password = createRef();
+  handlesubmit = async (e) => {
+    e.preventDefault();
 
-console.log('sumbit...!');
+    console.log(
+      "email:",
+      this.email.current.value,
+      "password:",
+      this.password.current.value
+    );
+
+    const person = {
+      email: this.email.current.value,
+      password: this.password.current.value,
+    };
+
+    if (person.email && person.password) {
+      const response = await axios.post("https://reqres.in/api/users", person);
+      console.log(response);
     }
+  };
   state = {};
   render() {
     return (
       <form onSubmit={this.handlesubmit}>
         <div className="mb-3">
           <label htmlFor="email">Email:</label>
-          <input id="email" className="form-control" type="text" />
+          <input
+            ref={this.email}
+            id="email"
+            className="form-control"
+            type="text"
+          />
           <label htmlFor="password">password:</label>
-          <input id="password" className="form-control" type="text" />
-          <button className="btn  btn-primary mt-3" type="submit" >submit</button>
+          <input
+            ref={this.password}
+            id="password"
+            className="form-control"
+            type="text"
+          />
+          <button className="btn  btn-primary mt-3" type="submit">
+            submit
+          </button>
         </div>
       </form>
     );
