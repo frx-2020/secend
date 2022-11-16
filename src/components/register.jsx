dd1fe96bfadab1a6a3057033ddb9fc7d031e2511
Farshid <xamanifar98@gmail.com>
2022-11-16 18:10:50 +0330

    create login panel &added e.preventDefault

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,9 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
 class Register extends Component {
-    state = {  } 
-    render() { 
-        return (<h1>Register</h1>);
+    handlesubmit =(e)=>{
+        e.preventDefault();
+    
+
+console.log('sumbit...!');
     }
+  state = {};
+  render() {
+    return (
+      <form onSubmit={this.handlesubmit}>
+        <div className="mb-3">
+          <label htmlFor="email">Email:</label>
+          <input id="email" className="form-control" type="text" />
+          <label htmlFor="password">password:</label>
+          <input id="password" className="form-control" type="text" />
+          <button className="btn  btn-primary mt-3" type="submit" >submit</button>
+        </div>
+      </form>
+    );
+  }
 }
- 
+
 export default Register;
