a4dafd4fae2b4130c7a31b698273ed39884d8c9a
Farshid <xamanifar98@gmail.com>
2022-11-24 10:22:03 +0330

    send token in headder request

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+axios.defaults.headers.common['token']='hello...';
 const Dashboard = () => {
+
+    useEffect(()=>{
+
+       async function x (){
+        const response=await axios.get('https://reqres.in/api/users');
+        console.log('token dash=',response.data);
+        }
+        x();
+        
+    },[])
     return ( <h1>Dashboard</h1> );
 }
  
